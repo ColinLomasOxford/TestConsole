5df61dfab8049d28e579ba3e3b5fd5e416e88e48
ColinLomasOxford <+Wd5vp.bc6>
2021-04-28 17:58:39 +0100

    Changed for Miia

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -6,7 +6,22 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i1 = Convert.ToInt32(Console.ReadLine());
+            int i2 = Convert.ToInt32(Console.ReadLine());
+            //int i3 = Convert.ToInt32(Console.ReadLine());
+            double a = miaFunc(i1, i2);
+            Console.WriteLine("answer is: " + a);
+        }
+
+        static double miaFunc(int i, int j)
+        {
+            return (double) i / j;
+        }
+
+        static double colFunc(int i1, int i2, int i3)
+        {
+            return i1 + i2 + i3;
         }
     }
+
 }
